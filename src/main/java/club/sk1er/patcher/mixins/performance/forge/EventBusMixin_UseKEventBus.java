ffbeeadfee5ca29b47b592215c12c43037e6248e
xtrm <oss@xtrm.me>
2024-07-19 22:01:09 +0200

    Fix name casing and better logging

--- src/main/java/club/sk1er/patcher/mixins/performance/forge/EventBusMixin_UseKEventBus.java
+++ src/main/java/club/sk1er/patcher/mixins/performance/forge/EventBusMixin_UseKEventBus.java
@@ -9,17 +9,18 @@ import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.*;
 
 @Mixin(value = EventBus.class, remap = false, priority = 0)
-public class EventBusMixin_UseKeventBus {
+public class EventBusMixin_UseKEventBus {
     @Shadow
     @Final
     private int busID;
+
     @Unique
     private final Logger patcher$logger = LogManager.getLogger();
+
     @Unique
     private final KEventBus patcher$kEventBus = new KEventBus(new DirectInvoker(), e -> {
-        e.printStackTrace();
-        patcher$logger.error("Failed to post event while using KEventBus added by PolyPatcher!");
-        patcher$logger.error("Exception: ", e);
+        patcher$logger.error("Failed to post event while using KEventBus added by PolyPatcher!", e);
+
         patcher$logger.error("This is likely not a KEventBus issue, but rather an issue with the event itself.");
         patcher$logger.error("To disable KEventBus and use the default debug logging from Forge, set the system property 'patcher.keventbus' to 'false'!");
         patcher$logger.error("Alternatively, contact us at https://polyfrost.org/discord");
