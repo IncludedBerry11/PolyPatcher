9bf8df9b233947c6e80abb56f5e502446446fd46
IncludedBerry11 <116087837+IncludedBerry11@users.noreply.github.com>
2024-08-28 12:39:44 +0530

    Update LibraryLWJGLOpenALImpl.java

--- src/main/java/club/sk1er/patcher/util/world/sound/audioswitcher/LibraryLWJGLOpenALImpl.java
+++ src/main/java/club/sk1er/patcher/util/world/sound/audioswitcher/LibraryLWJGLOpenALImpl.java
@@ -3,14 +3,11 @@ package club.sk1er.patcher.util.world.sound.audioswitcher;
 import club.sk1er.patcher.Patcher;
 import club.sk1er.patcher.config.PatcherConfig;
 import org.lwjgl.LWJGLException;
-import org.lwjgl.openal.AL;
 
 import java.util.List;
 
 @SuppressWarnings("unused")
 public class LibraryLWJGLOpenALImpl {
     public static void createAL() throws LWJGLException {
-      if (AL.isCreated()) AL.destroy();
-      AL.create();
     }
 }
